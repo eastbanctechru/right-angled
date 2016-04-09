a55dd67284a8706431e0640855d20aa37aff2d93
Fedor Shchudlo <fshchudlo@gmail.com>
2016-04-09 14:20:00 +0600

    sort change worked

--- app/e2e4-angular/e2e4sort.js
+++ app/e2e4-angular/e2e4sort.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'e2e4/src/common/defaults'], function(exports_1, context_1) {
+System.register(['angular2/core', 'e2e4/src/common/defaults', './ngListService', './ngPagedListService', './ngBufferedListService', 'e2e4/src/common/SortDirection'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,10 @@ System.register(['angular2/core', 'e2e4/src/common/defaults'], function(exports_
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, defaults_1;
+    var __param = (this && this.__param) || function (paramIndex, decorator) {
+        return function (target, key) { decorator(target, key, paramIndex); }
+    };
+    var core_1, defaults_1, ngListService_1, ngPagedListService_1, ngBufferedListService_1, SortDirection_1;
     var E2E4Sort;
     return {
         setters:[
@@ -19,17 +22,65 @@ System.register(['angular2/core', 'e2e4/src/common/defaults'], function(exports_
             },
             function (defaults_1_1) {
                 defaults_1 = defaults_1_1;
+            },
+            function (ngListService_1_1) {
+                ngListService_1 = ngListService_1_1;
+            },
+            function (ngPagedListService_1_1) {
+                ngPagedListService_1 = ngPagedListService_1_1;
+            },
+            function (ngBufferedListService_1_1) {
+                ngBufferedListService_1 = ngBufferedListService_1_1;
+            },
+            function (SortDirection_1_1) {
+                SortDirection_1 = SortDirection_1_1;
             }],
         execute: function() {
             E2E4Sort = (function () {
-                function E2E4Sort(el) {
-                    el.nativeElement.classList.add(defaults_1.Defaults.sortAttribute.sortableClassName);
+                function E2E4Sort(el, ngListService, ngPagedListService, ngBufferedListService) {
+                    this.listService = ngListService || ngPagedListService || ngBufferedListService;
+                    this.clickHandlerBinded = this.clickHandler.bind(this);
+                    this.checkElementClassesBinded = this.checkElementClasses.bind(this);
+                    this.nativeElement = el.nativeElement;
+                    this.nativeElement.classList.add(defaults_1.Defaults.sortAttribute.sortableClassName);
                 }
+                E2E4Sort.prototype.clickHandler = function (evt) {
+                    if (this.listService.ready) {
+                        this.listService.sortManager.setSort(this.columnName, evt.ctrlKey);
+                        this.listService.onSortChangesCompleted();
+                    }
+                };
+                E2E4Sort.prototype.ngOnChanges = function () {
+                    this.checkElementClassesBinded();
+                };
+                E2E4Sort.prototype.checkElementClasses = function () {
+                    var _this = this;
+                    var existedSortIndex = this.listService.sortManager.sortings ?
+                        this.listService.sortManager.sortings.findIndex(function (sp) { return sp.fieldName === _this.columnName; }) : -1;
+                    if (existedSortIndex !== -1) {
+                        var direction = this.listService.sortManager.sortings[existedSortIndex].direction;
+                        this.nativeElement.classList.remove(direction === SortDirection_1.SortDirection.Asc ? defaults_1.Defaults.sortAttribute.descClassName : defaults_1.Defaults.sortAttribute.ascClassName);
+                        this.nativeElement.classList.add(direction === SortDirection_1.SortDirection.Asc ? defaults_1.Defaults.sortAttribute.ascClassName : defaults_1.Defaults.sortAttribute.descClassName);
+                    }
+                    else {
+                        this.nativeElement.classList.remove(defaults_1.Defaults.sortAttribute.ascClassName, defaults_1.Defaults.sortAttribute.descClassName);
+                    }
+                };
+                __decorate([
+                    core_1.Input('e2e4-sort'), 
+                    __metadata('design:type', String)
+                ], E2E4Sort.prototype, "columnName", void 0);
                 E2E4Sort = __decorate([
                     core_1.Directive({
+                        host: {
+                            '(click)': 'clickHandlerBinded($event)'
+                        },
                         selector: '[e2e4-sort]'
-                    }), 
-                    __metadata('design:paramtypes', [core_1.ElementRef])
+                    }),
+                    __param(1, core_1.Optional()),
+                    __param(2, core_1.Optional()),
+                    __param(3, core_1.Optional()), 
+                    __metadata('design:paramtypes', [core_1.ElementRef, ngListService_1.NgListService, ngPagedListService_1.NgPagedListService, ngBufferedListService_1.NgBufferedListService])
                 ], E2E4Sort);
                 return E2E4Sort;
             }());
@@ -37,4 +88,4 @@ System.register(['angular2/core', 'e2e4/src/common/defaults'], function(exports_
         }
     }
 });
-//# sourceMappingURL=e2e4sort.js.map
+//# sourceMappingURL=e2e4Sort.js.map
--- app/e2e4-angular/e2e4sort.ts
+++ app/e2e4-angular/e2e4sort.ts
@@ -1,10 +1,51 @@
-import {Directive, ElementRef, Input} from 'angular2/core';
+import {Directive, ElementRef, Input, Optional, OnChanges} from 'angular2/core';
 import {Defaults} from 'e2e4/src/common/defaults';
+import {NgListService} from './ngListService';
+import {NgPagedListService} from './ngPagedListService';
+import {NgBufferedListService} from './ngBufferedListService';
+import {SortDirection} from 'e2e4/src/common/SortDirection';
+
+
 @Directive({
+    host: {
+        '(click)': 'clickHandlerBinded($event)'
+    },
     selector: '[e2e4-sort]'
 })
-export class E2E4Sort {
-    constructor(el: ElementRef) {
-        el.nativeElement.classList.add(Defaults.sortAttribute.sortableClassName);
+export class E2E4Sort implements OnChanges {
+    private nativeElement: HTMLElement;
+    listService: NgListService | NgPagedListService | NgBufferedListService;
+    private clickHandlerBinded: (event: MouseEvent) => any;
+    private checkElementClassesBinded: () => void;
+    @Input('e2e4-sort') columnName: string;
+    constructor(el: ElementRef,
+        @Optional() ngListService: NgListService,
+        @Optional() ngPagedListService: NgPagedListService,
+        @Optional() ngBufferedListService: NgBufferedListService) {
+        this.listService = ngListService || ngPagedListService || ngBufferedListService;
+        this.clickHandlerBinded = this.clickHandler.bind(this);
+        this.checkElementClassesBinded = this.checkElementClasses.bind(this);
+        this.nativeElement = el.nativeElement;
+        this.nativeElement.classList.add(Defaults.sortAttribute.sortableClassName);
+    }
+    clickHandler(evt: MouseEvent): void {
+        if (this.listService.ready) {
+            this.listService.sortManager.setSort(this.columnName, evt.ctrlKey);
+            this.listService.onSortChangesCompleted();
+        }
+    }
+    ngOnChanges(): void {
+        this.checkElementClassesBinded();
+    }
+    checkElementClasses(): void {
+        const existedSortIndex = this.listService.sortManager.sortings ?
+            this.listService.sortManager.sortings.findIndex(sp => sp.fieldName === this.columnName) : -1;
+        if (existedSortIndex !== -1) {
+            const direction = this.listService.sortManager.sortings[existedSortIndex].direction;
+            this.nativeElement.classList.remove(direction === SortDirection.Asc ? Defaults.sortAttribute.descClassName : Defaults.sortAttribute.ascClassName);
+            this.nativeElement.classList.add(direction === SortDirection.Asc ? Defaults.sortAttribute.ascClassName : Defaults.sortAttribute.descClassName);
+        } else {
+            this.nativeElement.classList.remove(Defaults.sortAttribute.ascClassName, Defaults.sortAttribute.descClassName);
+        }
     }
 }
