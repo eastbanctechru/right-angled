f289d92a6d2951a690574e8f3f69a31ec39c4c89
a-vain <a.vain@eastbanctech.ru>
2016-04-26 16:50:09 +0600

    routing added

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,20 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {PagedListSampleComponent} from './paged-list-sample/paged-list-sample.component';
 import {BufferedListSampleComponent} from './buffered-list-sample/buffered-list-sample.component';
 import {SimpleListSampleComponent} from './simple-list-sample/simple-list-sample.component';
 import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
-    directives: [ROUTER_DIRECTIVES, DROPDOWN_DIRECTIVES], selector: 'angular-app', templateUrl: 'app/app.html'
+    directives: [
+        ROUTER_DIRECTIVES,
+        DROPDOWN_DIRECTIVES
+    ],
+    selector: 'angular-app',
+    templateUrl: 'app/app.html',
+    providers: [
+        ROUTER_PROVIDERS
+    ]
 })
 @RouteConfig([
     { component: PagedListSampleComponent, name: 'PagedListSample', path: '/paged-list-sample', useAsDefault: true },
