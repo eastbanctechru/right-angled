5fa203a0ab2bc2693efdf67a9944b50d8599e5a0
Fedor Shchudlo <fshchudlo@gmail.com>
2016-05-03 15:32:12 +0500

    listen changes of takerowcount

--- app/rectangle/footer/buffered-controls/take-row-count.ts
+++ app/rectangle/footer/buffered-controls/take-row-count.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, KeyValueDiffers, KeyValueDiffer, DoCheck, OnDestroy} from 'angular2/core';
 import {RtList} from '../../lists/list';
 import {NgBufferedListService} from '../../bootstrap/ngBufferedListService';
 import {MISC_DIRECTIVES} from '../../misc-directives';
@@ -8,9 +8,21 @@ import {MISC_DIRECTIVES} from '../../misc-directives';
     selector: 'rt-take-row-count',
     template: '<input rt-select-on-focus type="text" maxlength="3" (keyup.enter)="onEnter($event)" [(ngModel)]="rowCount"/>'
 })
-export class RtTakeRowCount {
+export class RtTakeRowCount implements DoCheck, OnDestroy {
     bufferedListService: NgBufferedListService;
     private innerRowCount: number;
+    private serviceDiffer: KeyValueDiffer;
+    private checkRowCountChangedBinded: (item: any) => void;
+    constructor(listHost: RtList, differs: KeyValueDiffers) {
+        if (!listHost.isBufferedList) {
+            throw new Error('[rt-take-row-count] directive can be used only with buffered list services.');
+        }
+        this.bufferedListService = <NgBufferedListService>listHost.serviceInstance;
+        this.innerRowCount = this.bufferedListService.takeRowCount;
+        this.serviceDiffer = differs.find([]).create(null);
+        this.checkRowCountChangedBinded = this.checkRowCountChange.bind(this);
+    }
+
     get rowCount(): any {
         return this.innerRowCount;
     }
@@ -22,12 +34,19 @@ export class RtTakeRowCount {
         this.bufferedListService.takeRowCount = value;
         setTimeout(() => this.innerRowCount = this.bufferedListService.takeRowCount);
     }
-    constructor(listHost: RtList) {
-        if (!listHost.isBufferedList) {
-            throw new Error('[rt-take-row-count] directive can be used only with buffered list services.');
+    checkRowCountChange(item: any): void {
+        if (item.key === 'takeRowCountInternal' && item.currentValue!==this.innerRowCount) {
+            this.innerRowCount = item.currentValue;
         }
-        this.bufferedListService = <NgBufferedListService>listHost.serviceInstance;
-        this.innerRowCount = this.bufferedListService.takeRowCount;
+    }
+    ngOnDestroy(): void {
+        delete this.checkRowCountChangedBinded;
+    }
+    ngDoCheck(): void {
+        let serviceDiff = this.serviceDiffer.diff(this.bufferedListService);
+        if (serviceDiff) {
+            serviceDiff.forEachChangedItem(this.checkRowCountChangedBinded);
+        }
     }
     onEnter(evt: KeyboardEvent): void {
         this.innerRowCount = this.bufferedListService.takeRowCount;
