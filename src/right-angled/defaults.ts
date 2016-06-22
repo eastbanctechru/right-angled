34c7a962f59b5ba0ffff46551dbf6acb8be238f8
Fedor Shchudlo <fshchudlo@gmail.com>
2016-06-22 11:22:31 +0600

    dynamic rebdering of total-records-text implemented

--- src/right-angled/defaults.ts
+++ src/right-angled/defaults.ts
@@ -8,10 +8,7 @@ export class Defaults {
         sortable: 'rt-sortable'
     };
     static messages = {
-        bufferedListTotalRecordsText: 'Loaded records {0} of {1} ',
         loadButtonCancelRequest: 'Press to stop data loading',
-        loadButtonLoad: 'Load data',
-        pagedListTotalRecordsText: 'Displaying records {0} - {1} of {2}.',
-        simpleListTotalRecordsText: '{0} records displayed'
+        loadButtonLoad: 'Load data'
     };
 }
--- src/right-angled/footer/total-records-text.ts
+++ src/right-angled/footer/total-records-text.ts
@@ -6,42 +6,29 @@ import {Defaults} from '../defaults';
 
 @Component({
     selector: 'rt-total-records-text',
-    template: `<span *ngIf="isVisible">{{text}}</span>`
+    template: `<template *ngIf="isVisible"
+            [ngTemplateOutlet]="templateLink"
+            [ngOutletContext]="{pager: listHost.serviceInstance.pager}">
+        </template>`
 })
 export class RtTotalRecordsTextComponent implements DoCheck, OnInit {
-    static formatString(format: string, ...args: any[]): string {
-        let s = arguments[0];
-        for (let i = 0; i < arguments.length - 1; i++) {
-            let reg = new RegExp('\\{' + i + '\\}', 'gm');
-            s = s.replace(reg, arguments[i + 1]);
-        }
-        return s;
-    }
     listDiffer: KeyValueDiffer;
-    selfDiffer: KeyValueDiffer;
     pagerDiffer: KeyValueDiffer;
-    @Input('text') textInput;
+    @Input('templateLink') templateLink;
     isVisible: boolean;
-    text: string;
     listHost: RtListComponent;
     constructor(listHost: RtListComponent, differs: KeyValueDiffers) {
         this.listDiffer = differs.find([]).create(null);
-        this.selfDiffer = differs.find([]).create(null);
         this.pagerDiffer = differs.find([]).create(null);
         this.listHost = listHost;
     }
     ngOnInit(): void {
         this.setVisibility();
-        this.setDisplayText();
     }
     ngDoCheck(): void {
         let listDiff = this.listDiffer.diff(this.listHost.serviceInstance);
         if (listDiff) {
             listDiff.forEachChangedItem(this.checkListChanges);
-        }
-        let selfDiff = this.selfDiffer.diff(this);
-        if (selfDiff) {
-            selfDiff.forEachChangedItem(this.checkSelfChanges);
         }
         let pagerDiff = this.pagerDiffer.diff(this.listHost.serviceInstance.pager);
         if (pagerDiff) {
@@ -56,23 +43,6 @@ export class RtTotalRecordsTextComponent implements DoCheck, OnInit {
     checkPagerChanges = (item: any): void => {
         if (item.key === 'totalCount') {
             this.setVisibility();
-        }
-        if (item.key === 'loadedCount') {
-            this.setDisplayText();
-        }
-    }
-    checkSelfChanges = (item: any): void => {
-        if (item.key === 'text') {
-            this.setDisplayText();
-        }
-    }
-    setDisplayText(): void {
-        if (this.listHost.isSimpleList) {
-            this.text = RtTotalRecordsTextComponent.formatString(this.textInput || Defaults.messages.simpleListTotalRecordsText, this.listHost.serviceInstance.pager.loadedCount);
-        } else if (this.listHost.isBufferedList) {
-            this.text = RtTotalRecordsTextComponent.formatString(this.textInput || Defaults.messages.bufferedListTotalRecordsText, this.listHost.serviceInstance.pager.loadedCount, this.listHost.serviceInstance.pager.totalCount);
-        } else if (this.listHost.isPagedList) {
-            this.text = RtTotalRecordsTextComponent.formatString(this.textInput || Defaults.messages.pagedListTotalRecordsText, (<NgPagedListService>this.listHost.serviceInstance).pager.displayFrom, (<NgPagedListService>this.listHost.serviceInstance).pager.displayTo, this.listHost.serviceInstance.pager.totalCount);
         }
     }
     setVisibility(): void {
