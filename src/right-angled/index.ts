0adced662800ea5954ebc3be4c9e889e74cc0a2c
Fedor Shchudlo <fshchudlo@gmail.com>
2016-07-19 14:09:32 +0600

    couple of fixes

--- src/right-angled/index.ts
+++ src/right-angled/index.ts
@@ -9,8 +9,8 @@ import { LIST_DIRECTIVES } from './list-directives/index';
 import { BUFFERED_FOOTER_DIRECTIVES, PAGED_FOOTER_DIRECTIVES, REGULAR_FOOTER_DIRECTIVES } from './list-directives/index';
 
 export var PAGED_LIST_DIRECTIVES: any[] = LIST_DIRECTIVES.concat(PAGED_FOOTER_DIRECTIVES.concat(SELECTION_DIRECTIVES));
-export var BUFFERED_LIST_DIRECTIVES: any[] = BUFFERED_FOOTER_DIRECTIVES.concat(PAGED_FOOTER_DIRECTIVES.concat(SELECTION_DIRECTIVES));
-export var REGULAR_LIST_DIRECTIVES: any[] = REGULAR_FOOTER_DIRECTIVES.concat(PAGED_FOOTER_DIRECTIVES.concat(SELECTION_DIRECTIVES));
+export var BUFFERED_LIST_DIRECTIVES: any[] = BUFFERED_FOOTER_DIRECTIVES.concat(BUFFERED_FOOTER_DIRECTIVES.concat(SELECTION_DIRECTIVES));
+export var REGULAR_LIST_DIRECTIVES: any[] = REGULAR_FOOTER_DIRECTIVES.concat(REGULAR_FOOTER_DIRECTIVES.concat(SELECTION_DIRECTIVES));
 
 import { NgQueryStringStateService } from './services/ng-query-string-state-service';
 import { NgPagedListService } from './services/ng-paged-list-service.service';
