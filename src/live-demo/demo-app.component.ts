d9127403f24fdb2985bf6d8fa9fc010b7fa2efa0
a-vain <a.vain@eastbanctech.ru>
2016-07-28 13:46:33 +0700

    demo styles

--- src/live-demo/demo-app.component.ts
+++ src/live-demo/demo-app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 import 'rxjs/Rx';
 import { AirportsService } from './shared';
 
 @Component({
-  directives: [ROUTER_DIRECTIVES, DROPDOWN_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES],
   moduleId: module.id,
   providers: [AirportsService],
   selector: 'rt-demo-app',
