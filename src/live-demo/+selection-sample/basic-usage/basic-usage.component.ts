d8db596419bb936e7029c1d9829e1b13d9e2378b
Fedor Shchudlo <fshchudlo@gmail.com>
2016-08-28 15:58:04 +0700

    chore(selection): remove unused imports

--- src/live-demo/+selection-sample/basic-usage/basic-usage.component.ts
+++ src/live-demo/+selection-sample/basic-usage/basic-usage.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { SHARED_DIRECTIVES } from '../../shared';
 
 import { AirportsService } from '../../shared';
 
 @Component({
-    directives: [SHARED_DIRECTIVES],
     moduleId: module.id,
     selector: 'rt-demo-basic-usage',
     templateUrl: 'basic-usage.component.html'
 })
 export class BasicUsageComponent {
-    public regions: Observable<any>;
+    public regions: any;
     constructor(public airportsService: AirportsService) {
         this.regions = this.airportsService.getSelectableRegions();
     }
