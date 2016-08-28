fee87fff7a19bd2bb13315ae28b95fd1c013d495
Fedor Shchudlo <fshchudlo@gmail.com>
2016-08-28 16:30:41 +0700

    feat(demo): more elegant use of prettyprint

--- src/live-demo/+selection-sample/selection-sample.component.ts
+++ src/live-demo/+selection-sample/selection-sample.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 import { BasicUsageComponent } from './basic-usage/basic-usage.component';
 
@@ -8,13 +9,21 @@ import { BasicUsageComponent } from './basic-usage/basic-usage.component';
     templateUrl: 'selection-sample.component.html'
 })
 export class SelectionSampleComponent {
-    public HOW_TO_USE_TS: string = `
+    public BASIC_USAGE_TS: any;
+    public HOW_TO_USE_TS: string;
+    constructor(http: Http) {
+        this.BASIC_USAGE_TS =
+            http.get('https://raw.githubusercontent.com/fshchudlo/right-angled/master/src/live-demo/%2Bselection-sample/basic-usage/basic-usage.component.ts')
+                .map(res => {
+                    return typeof PR !== 'undefined' ? PR.prettyPrintOne(res.text(), 'js') : 'Prettyprint is broken for samo reason.';
+                });
+        this.HOW_TO_USE_TS = typeof PR !== 'undefined' ? PR.prettyPrintOne(`
     import { RTSelectionModule } from 'right-angled';
     @NgModule({
         bootstrap: [AppComponent],
         declarations: [AppComponent],
         imports: [...RTSelectionModule...]
     })
-    export class AppModule {}
-`;
+    export class AppModule {}`, 'js') : 'Prettyprint is broken for samo reason.';
+    }
 }
--- src/live-demo/demo-app.component/demo-app.component.ts
+++ src/live-demo/demo-app.component/demo-app.component.ts
@@ -1,11 +1,7 @@
-import { AfterContentInit, Component, ViewEncapsulation } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
 import 'rxjs/Rx';
 
 import { AirportsService } from '../shared';
-
-// google code-prettify
-declare const PR: any;
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -15,19 +11,5 @@ declare const PR: any;
   styleUrls: ['demo-app.component.css'],
   templateUrl: 'demo-app.component.html'
 })
-export class DemoAppComponent implements AfterContentInit {
-  constructor(private router: Router) {
-
-  }
-  public ngAfterContentInit(): any {
-
-    this.router.events.subscribe((event: any) => {
-      if (event instanceof NavigationEnd) {
-        if (typeof PR !== 'undefined') {
-          // google code-prettify
-          setTimeout(PR.prettyPrint, 50);
-        }
-      }
-    });
-  }
+export class DemoAppComponent {
 }
