50fe32b5efac6a137377582cf1697088262ebd48
Fedor Shchudlo <fshchudlo@gmail.com>
2016-08-31 15:34:57 +0700

    chore(package): 0.12.3

--- src/components/misc-directives/focus-on-render.directive.ts
+++ src/components/misc-directives/focus-on-render.directive.ts
@@ -3,13 +3,12 @@ import { AfterViewInit, Directive, ElementRef, Renderer } from '@angular/core';
 @Directive({
     selector: 'input[rtFocusOnRender]'
 })
-export class FocusOnRenderDirective  implements AfterViewInit{
+export class FocusOnRenderDirective implements AfterViewInit {
     private nativeEl: HTMLInputElement;
     constructor(private renderer: Renderer, elementRef: ElementRef) {
         this.nativeEl = elementRef.nativeElement;
     }
     public ngAfterViewInit(): void {
-        console.log('focus');
         this.renderer.invokeElementMethod(this.nativeEl, 'focus');
     }
 }
