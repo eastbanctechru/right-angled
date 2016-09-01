ad3550aad8a95c999e1815835e0a75be8a9064fb
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-01 16:29:10 +0700

    feat(demo): correct scrolling and styles for demo pages

--- src/components/misc-directives/focus-on-render.directive.ts
+++ src/components/misc-directives/focus-on-render.directive.ts
@@ -1,14 +1,15 @@
-import { AfterViewInit, Directive, ElementRef, Renderer } from '@angular/core';
+import { AfterContentInit, Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
     selector: 'input[rtFocusOnRender]'
 })
-export class FocusOnRenderDirective implements AfterViewInit {
+export class FocusOnRenderDirective implements AfterContentInit {
     private nativeEl: HTMLInputElement;
     constructor(private renderer: Renderer, elementRef: ElementRef) {
         this.nativeEl = elementRef.nativeElement;
     }
-    public ngAfterViewInit(): void {
-        this.renderer.invokeElementMethod(this.nativeEl, 'focus');
+    public ngAfterContentInit(): void {
+        // we need set timeout for the cases when element itself is rendered by *ngIf directive and we need to wait it's rendering
+        setTimeout(() => this.renderer.invokeElementMethod(this.nativeEl, 'focus'), 0);
     }
 }
