eb076ee4023d1c76b36bbd724ea264e161832067
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-17 15:29:47 +0700

    feat(misc directives): `events` argument can be simple string now

--- src/components/misc-directives/events-attacher.base.ts
+++ src/components/misc-directives/events-attacher.base.ts
@@ -1,17 +1,20 @@
 import { ElementRef, OnChanges, OnDestroy, SimpleChange } from '@angular/core';
 
 export abstract class EventsAttacherBase implements OnChanges, OnDestroy {
-    public eventNames: Array<string>;
+    public eventNames: string | Array<string>;
     constructor(private elementRef: ElementRef, private eventHandler: EventListener | EventListenerObject) {
     }
     public ngOnChanges(changes: { eventNames?: SimpleChange }): void {
         if (changes.eventNames) {
-            this.removeListeners(changes.eventNames.previousValue);
-            this.addListeners(changes.eventNames.currentValue);
+            this.removeListeners(this.adjustEvents(changes.eventNames.previousValue));
+            this.addListeners(this.adjustEvents(changes.eventNames.currentValue));
         }
     }
+    private adjustEvents(eventsNames: string | Array<string>): Array<string> {
+        return eventsNames ? Array.isArray(eventsNames) ? eventsNames : [eventsNames] : [];
+    }
     public ngOnDestroy(): void {
-        this.removeListeners(this.eventNames);
+        this.removeListeners(this.adjustEvents(this.eventNames));
     }
     private removeListeners(eventNames: string[]): void {
         if (!eventNames || !eventNames.length) {
