ac82d5fd3740cc58c6b55023e628ea39735af3a5
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-17 16:01:04 +0700

    feat(demo): rtSelectOnFocus

--- src/live-demo/+misc-directives-sample/misc-directives-sample.module.ts
+++ src/live-demo/+misc-directives-sample/misc-directives-sample.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { MiscDirectivesSampleComponent } from './misc-directives-sample.component';
 import { PreventDefaultsComponent } from './prevent-defaults/prevent-defaults.component';
+import { SelectOnFocusComponent } from './select-on-focus/select-on-focus.component';
 import { StopEventsComponent } from './stop-events/stop-events.component';
 
 @NgModule({
-    declarations: [MiscDirectivesSampleComponent, StopEventsComponent, PreventDefaultsComponent],
-    exports: [MiscDirectivesSampleComponent, StopEventsComponent, PreventDefaultsComponent],
+    declarations: [MiscDirectivesSampleComponent, SelectOnFocusComponent, StopEventsComponent, PreventDefaultsComponent],
+    exports: [MiscDirectivesSampleComponent, SelectOnFocusComponent, StopEventsComponent, PreventDefaultsComponent],
     imports: [CommonModule, SharedModule]
 })
 export class MiscDirectivesSampleModule { }
---
+++ src/live-demo/+misc-directives-sample/select-on-focus/select-on-focus.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'rt-demo-select-on-focus',
+    template: `<input class="form-control" type="text" rtSelectOnFocus value="focus me" />`
+})
+export class SelectOnFocusComponent {
+}
