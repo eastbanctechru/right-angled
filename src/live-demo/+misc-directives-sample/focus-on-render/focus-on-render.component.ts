ad0957d37aa00648559d607a15e336589f1b1344
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-17 16:11:08 +0700

    feat(demo): rtFocusOnRender

---
+++ src/live-demo/+misc-directives-sample/focus-on-render/focus-on-render.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'rt-demo-focus-on-render',
+    templateUrl: 'focus-on-render.component.html'
+})
+export class FocusOnRenderComponent {
+    public renderInput: boolean = false;
+}
--- src/live-demo/+misc-directives-sample/misc-directives-sample.module.ts
+++ src/live-demo/+misc-directives-sample/misc-directives-sample.module.ts
@@ -2,14 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
+import { FocusOnRenderComponent } from './focus-on-render/focus-on-render.component';
 import { MiscDirectivesSampleComponent } from './misc-directives-sample.component';
 import { PreventDefaultsComponent } from './prevent-defaults/prevent-defaults.component';
 import { SelectOnFocusComponent } from './select-on-focus/select-on-focus.component';
 import { StopEventsComponent } from './stop-events/stop-events.component';
 
 @NgModule({
-    declarations: [MiscDirectivesSampleComponent, SelectOnFocusComponent, StopEventsComponent, PreventDefaultsComponent],
-    exports: [MiscDirectivesSampleComponent, SelectOnFocusComponent, StopEventsComponent, PreventDefaultsComponent],
+    declarations: [MiscDirectivesSampleComponent, FocusOnRenderComponent, SelectOnFocusComponent, StopEventsComponent, PreventDefaultsComponent],
+    exports: [MiscDirectivesSampleComponent, FocusOnRenderComponent, SelectOnFocusComponent, StopEventsComponent, PreventDefaultsComponent],
     imports: [CommonModule, SharedModule]
 })
 export class MiscDirectivesSampleModule { }
