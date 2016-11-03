8af3d1d14831e706fd403d8684029afb1a622d45
Fedor Shchudlo <fshchudlo@gmail.com>
2016-11-03 22:48:45 +0700

    test(RowNumberPipe): tests provided

---
+++ tests/list-directives/row-number-pipe.ts
@@ -0,0 +1,30 @@
+import { RowNumberPipe } from '../../src/list-directives';
+
+describe('TitleCasePipe', () => {
+    let listStub: any = {
+        listService: {
+            pager: {
+            }
+        }
+    };
+    let pipe = new RowNumberPipe();
+    it('Returns index + displayFrom if \'displayFrom\' property specified', () => {
+        expect(pipe.transform(0, <any>{
+            listService: {
+                pager: {
+                    displayFrom: 10
+                }
+            }
+        })).toBe(10);
+    });
+    it('Returns index + 1 if \'displayFrom\' property not specified', () => {
+        expect(pipe.transform(0, listStub)).toBe(1);
+    });
+    it('Throws error if rtListDirective is not specified', () => {
+        expect(() => pipe.transform(0, null)).toThrowError('Invalid value provided for parameter "rtList" of rtRowNumber pipe . Must be "rtList" directive instance.');
+    });
+    it('Throws error if index parameter is not numeric', () => {
+        expect(() => pipe.transform(null, listStub)).toThrowError('Invalid input value for rtRowNumber pipe. Must be a valid numeric value.');
+        expect(() => pipe.transform(<any>'abc', listStub)).toThrowError('Invalid input value for rtRowNumber pipe. Must be a valid numeric value.');
+    });
+});
